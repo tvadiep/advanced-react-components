f7ca2f6c783dab1252ac68175ed3944c5a9d108c
Diep Tran <diep.trv@gmail.com>
2023-06-11 22:23:31 +0700

    feat: add button group

---
+++ src/Radio/index.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import "./styles.css";
+
+export const RadioGroup = ({ onChange, selected, children }) => {
+  const RadioOptions = React.Children.map(children, (child) => {
+    return React.cloneElement(child, {
+      checked: child.props.value === selected,
+      onChange,
+    });
+  });
+
+  return <div className="RadioGroup">{RadioOptions}</div>;
+};
+
+export const RadioOption = ({ value, checked, onChange, children }) => {
+  return (
+    <div className="RadioOption">
+      <input
+        id={value}
+        type="radio"
+        name={value}
+        value={value}
+        checked={checked}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      <label htmlFor={value}>{children}</label>
+    </div>
+  );
+};
